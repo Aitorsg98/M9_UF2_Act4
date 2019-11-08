1e95d25c52bcdbfc752b46df97bf070d0812ede3
formu <formu@ASUS-LAPTOP>
2019-11-08 16:53:45 +0100

    1.2

--- src/uf2/Apartat1.java
+++ src/uf2/Apartat1.java
@@ -1,8 +1,6 @@
 package uf2;
 
 import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -18,13 +16,29 @@ public class Apartat1 {
 
 		@Override
 		public void run() {
-			// Thread.sleep();
+			
+			for (int ii = 0; ii < articles.length; ii++) {
+				articles[ii] = randomFill();
+				System.out.print("Client " + numeroClient + " ");
+				System.out.print("article " + (ii+1) + "/" + articles.length + " ");
+				if (ii+1 < articles.length) {
+					System.out.println( "(" + articles[ii] + " segons)...");
+				} else {
+					System.out.println( "(" + articles[ii] + " segons)...FINALITZAT");
+				}
+				try {
+					Thread.sleep(articles[ii] * 1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
 	public static int randomFill() {
 		double randomDouble = Math.random();
-		randomDouble = randomDouble * 30 + 1;
+		randomDouble = randomDouble * 7 + 2;
 		int randomInt = (int) randomDouble;
 		return randomInt;
 	}
@@ -32,41 +46,27 @@ public class Apartat1 {
 	public static void main(String[] args) {
 		int numClient = 1;
 
-		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(1);
+		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
 
 		for (int i = 0; i < 50; i++) {
 			double randomDouble = Math.random();
 			randomDouble = randomDouble * 30 + 1;
 			int randomInt = (int) randomDouble;
 
-			/*int[] articles = new int[randomInt];
-			for (int ii = 0; ii < articles.length; ii++) {
-				articles[ii] = randomFill();
-			}
-			
-			Client client = new Client(numClient, articles);
-
-			System.out.println("Creat el client " + numClient + " amb " + articles.length + " articles"+Arrays.toString(articles));*/
-			
 			int[] articles = new int[randomInt];
-			
-			Client client = new Client(numClient, articles);
-
-			System.out.println("Creat el client " + numClient + " amb " + articles.length + " articles");
-			System.out.println("Client " + numClient +  " passa per caixa...");
-
 			for (int ii = 0; ii < articles.length; ii++) {
-				long inici = System.currentTimeMillis() / 1000;
 				articles[ii] = randomFill();
-				System.out.print("Client " + numClient + " ");
-				System.out.print("article " + (ii+1) + "/" + articles.length + " ");
-				if (ii+1 < articles.length) {
-				System.out.println( "(" + articles[ii] + " segons)...");
-				} else {
-					System.out.println( "(" + articles[ii] + " segons)...FINALITZAT");
-				}
 			}
+
+			Client client = new Client(numClient, articles);
+			System.out.println("Creat el client " + numClient + " amb " + articles.length + " articles" + Arrays.toString(articles));
+
+			executor.execute(client);
+
 			numClient++;
 		}
+
+		executor.shutdown();
+
 	}
 }
